554624cb5bf3c4710a13fae991650beb4d72d38e
bom\bomi <kk3p1q1@gmail.com>
2021-12-14 23:05:40 +0900

    예매이력 메뉴 껍데기

--- src/pages/MyPage/ReserveHistory.tsx
+++ src/pages/MyPage/ReserveHistory.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Menu } from "antd";
-import { MailOutlined, AppstoreOutlined } from "@ant-design/icons";
+import { Descriptions } from "antd";
 
 function ReserveHistory() {
   const [menuItemKey, setMenuItemKey] = React.useState<string>("movie");
@@ -9,7 +8,17 @@ function ReserveHistory() {
     setMenuItemKey(e.key);
   };
 
-  return <React.Fragment>ReserveHistory</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Descriptions title="영화 제목">
+        <Descriptions.Item label="UserName">영화관명/몇 관</Descriptions.Item>
+        <Descriptions.Item label="Telephone">영화날짜</Descriptions.Item>
+        <Descriptions.Item label="Live">영화시간</Descriptions.Item>
+        <Descriptions.Item label="Remark">10,000원</Descriptions.Item>
+        <Descriptions.Item label="Address">몇포인트 적립</Descriptions.Item>
+      </Descriptions>
+    </React.Fragment>
+  );
 }
 
 export default ReserveHistory;
