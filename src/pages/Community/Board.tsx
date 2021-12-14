70aae6a7de9859d32a4e25d8f2f0c203f263424b
bom\bomi <kk3p1q1@gmail.com>
2021-12-14 23:01:05 +0900

    자유게시판 껍데기

--- src/pages/Community/Board.tsx
+++ src/pages/Community/Board.tsx
@@ -1,15 +1,60 @@
 import React from "react";
-import { Menu } from "antd";
-import { MailOutlined, AppstoreOutlined } from "@ant-design/icons";
+import { PageHeader, Space, Table, Tag } from "antd";
 
 function Board() {
-  const [menuItemKey, setMenuItemKey] = React.useState<string>("movie");
+  const columns = [
+    {
+      title: "글 번호",
+      dataIndex: "index",
+      key: "index",
+      width: 100,
+    },
+    {
+      title: "제목",
+      dataIndex: "title",
+      key: "title",
+      width: 450,
+    },
+    {
+      title: "작성자",
+      dataIndex: "writer",
+      key: "writer",
+    },
+    {
+      title: "조회수",
+      dataIndex: "viewCount",
+      key: "viewCount",
+      width: 100,
+    },
+  ];
 
-  const handlMenuOnClick = (e: any) => {
-    setMenuItemKey(e.key);
-  };
+  const data = [
+    {
+      index: "3",
+      title: "bad",
+      writer: "John Brown",
+      viewCount: 21,
+    },
+    {
+      index: "2",
+      title: "fdsfd",
+      writer: "Jim Green",
+      viewCount: 33,
+    },
+    {
+      index: "1",
+      title: "titltlel",
+      writer: "Joe Black",
+      viewCount: 13,
+    },
+  ];
 
-  return <React.Fragment>Board</React.Fragment>;
+  return (
+    <React.Fragment>
+      <PageHeader className="site-page-header" title="자유게시판" />
+      <Table columns={columns} dataSource={data} />
+    </React.Fragment>
+  );
 }
 
 export default Board;
