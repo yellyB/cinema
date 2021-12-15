c20f72d1e9a9baf4c08355d981ebac5c955871e4
bomoto <bomoto@fashionplus.co.kr>
2021-12-15 17:32:58 +0900

    예매 단계 껍데기 작성

---
+++ src/pages/Cinema/StepThree.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StepThree = () => {
+  return <>~~결제~~</>;
+};
+
+export { StepThree };
---
+++ src/pages/Cinema/StepTwo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StepTwo = () => {
+  return <>~~좌석 선택~~</>;
+};
+
+export { StepTwo };
