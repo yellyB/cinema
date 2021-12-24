f2e949777bfa820c1fe0cfeab998712110df8a88
bomiKim <kk3p1q1@gmail.com>
2021-12-24 14:25:02 +0900

    리덕스 알람 기능(조회만)

---
+++ src/reducers/index.tsx
@@ -0,0 +1,30 @@
+import * as types from "../actions/ActionTypes";
+import { IAlarm } from "../common/interface";
+
+const alarm: IAlarm[] = [
+  {
+    index: 0,
+    title: "알림",
+    content: "그렇습니다.",
+  },
+  {
+    index: 0,
+    title: "경고!",
+    content: "경고쓰.",
+  },
+];
+
+const counter = (state = alarm, action) => {
+  switch (action.type) {
+    case types.INCREMENT:
+    //   return { ...state, number: state.number + 1 };
+    case types.DECREMENT:
+    //   return { ...state, number: state.number - 1 };
+    case types.SET_COLOR:
+      return { ...state, color: action.color };
+    default:
+      return state;
+  }
+};
+
+export default counter;
